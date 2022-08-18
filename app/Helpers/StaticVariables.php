3b9aa513ca63fe2352fd171de58b49a7eb72418f
Kolawole <kolakachi@gmail.com>
2022-08-18 18:35:03 +0100

    create test cases

---
+++ app/Helpers/StaticVariables.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace App\Helpers;
+
+class StaticVariables {
+    
+    const STATIC_KEY = "720DF6C2482218518FA20FDC52D4DED7ECC043AB";
+}
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Helpers\StaticVariables;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_add_user_view()
+    {
+        $response = $this->get('/append-comment');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_api_append_user_comment()
+    {
+        $response = $this->post('/api/update', [
+            'id' => 4, 
+            'comment' => 'New user update',
+            'password' => StaticVariables::STATIC_KEY
+        ]);
+ 
+        $response->assertStatus(200);
+    }
+
+    public function test_api_invalid_user_id()
+    {
+        $response = $this->post('/api/update', [
+            'id' => "test", 
+            'comment' => 'New user update',
+            'password' => StaticVariables::STATIC_KEY
+        ]);
+ 
+        $response->assertStatus(422);
+        $response->assertSessionHasErrors();
+    }
+
+    public function test_api_invalid_password()
+    {
+        $response = $this->post('/api/update', [
+            'id' => 4, 
+            'comment' => 'New user update',
+            'password' => '720DF218518FA20FDC52D4DED7ECC043AB'
+        ]);
+ 
+        $response->assertStatus(401);
+    }
+}
