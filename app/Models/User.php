4d296450b01b310fa539c25ed0375ebd23db4ba7
Kolawole <kolakachi@gmail.com>
2022-08-18 14:10:16 +0100

    setup migration and seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    public $timestamps = false;
+
     /**
      * The attributes that are mass assignable.
      *
@@ -19,8 +21,7 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
-        'email',
-        'password',
+        'comments',
     ];
 
     /**
